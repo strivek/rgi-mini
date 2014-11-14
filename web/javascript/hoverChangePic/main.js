5b96e5d31c0e14b411a7fd283a004d7312efbbaa
chengyuan <chengyuan9826@qq.com>
2014-11-14 19:42:35 +0800

    修改

--- web/javascript/hoverChangePic/main.js
+++ web/javascript/hoverChangePic/main.js
@@ -4,64 +4,58 @@ require(["jquery","hoverChangePic/jquery.lightbox-0.5"], function () {
    var $block=$mn_f.find(".block"); //获取block块
    var $Cradio=$block.find(".radio");//点击按钮
    var $hd_show=$(".header .head_show");
-   var $menu= $hd_show.find(".radio");
-   var $s_rg=$hd_show.find(".slide_rg");
+    var $menu= $hd_show.find(".radio");
+    var $logo=$hd_show.find(".logo");
+    var $s_rg=$hd_show.find(".slide_rg");
     var $hd_link=$hd_show.find(".link");
     var $mid=$(".middle .f_wrap5");//获取logo块
     var $logoBlock=$mid.find(".LogoBlock");
     var $hd_list=$main.find(".header_list");
-    var $t_con=$hd_list.find(".con");
-    var $t_h1=$hd_list.find(".tit_h1");
-    var $t_h2=$hd_list.find(".tit_h2");
     var $hd_h1=$hd_show.find(".title");
-    var $d_radio=$t_con.find(".d_radio");
+    var $t_h1=$hd_list.find(".tit_h1");
+    var $t_tex=$hd_list.find(".tex");
+    var $ydhl=$hd_list.find(".ydhl");
+    var $d_radio=$hd_list.find(".d_radio");
 
-    $("body").animate({'scrollTop':0},1000);
+   /* $("body").animate({'scrollTop':0},1000);*/
 
     //点击页面顶部按钮，页面下拉
     $d_radio.on("click",function(){
     $("body").animate({'scrollTop':800},1000);
-
-
     });
 
     //顶部菜单按钮，单击展开下拉菜单
     $menu.on("click",function(){
-
         if($hd_list.height()==0)
         { $(this).find(".icon").hide().siblings(".icon2").show(); //鼠标点击，按钮旋转90度
-            $s_rg.animate({"margin-top":"33px","color":"#fff"},//褐色rgi_brand 文字下落
-            function(){
-                   //白色文字接着下落，同时改变颜色字体
-                    $t_h1.css({"margin-top":"0px","font-size":"30px","color":"#fff"});
-                   //下拉框文字“移动互联时代的海豚梦”逐渐显示
-                    $t_h2.fadeIn(1000);
-            });
+            $logo.css({"left":"20px","margin-left":"0"});//瑞观logo移动到左边
             $hd_h1.fadeOut(1000); //页面顶部文字“移动互联时代的海豚梦”逐渐隐藏
 
             $hd_list.animate({"height":"291px"},function(){ //下拉框的高度逐渐变大
-                $t_con.css({"opacity":100});//下拉框中的三段文字逐渐显示
+                $t_h1.animate({"margin-top":"42px"},function(){
+                    $t_tex.animate({"margin-top":"16px"},function(){//下拉框中的yi段文字逐渐显示
+                        $d_radio.animate({"margin-top":"28px"});
+                    });
+                });
             });
             $hd_link.css({"right":"16%","opacity":100});//页面顶部的两个导航从右边划入溅显
-
         }
         else{
             $(this).find(".icon2").hide().siblings(".icon").show(); //鼠标点击，按钮旋转90度
-            $s_rg.animate({"margin-top":"10px","color":"#92818d"},function(){//褐色rgi_brand 文字上行
-                $t_h1.css({"margin-top":"-72px","font-size":"14px","color":"#92818d"});//白色文字接着上滑，同时改变颜色字体
-                $t_h2.fadeOut();// //下拉框文字“移动互联时代的海豚梦”逐渐消失
-            })
+            $logo.css({"left":"50%","margin-left":"-97px"});
             $hd_h1.fadeIn(1000);//页面顶部文字“移动互联时代的海豚梦”逐渐显示
-            $hd_list.animate({"height":0},function(){//下拉框的高度逐渐变小
-                $t_con.css({"opacity":0});//下拉框中的三段文字逐渐消失
+            $d_radio.animate({"margin-top":"-235px"},function(){
+                $t_tex.animate({"margin-top":"-200px"},function(){//下拉框中的三段文字逐渐消失
+                    $t_h1.animate({"margin-top":"-72px"},function(){
+                        $hd_list.animate({"height":0});
+                    });
+                });
             });
             $hd_link.css({"right":"-16%","opacity":0});//页面顶部的两个导航从左边滑出
-
         }
-
     });
 
-//点击放大图片效果
+   //点击放大图片效果
     $Cradio.lightBox();
 
    //鼠标移入彩色图片显示，灰色图片隐藏
@@ -69,19 +63,24 @@ require(["jquery","hoverChangePic/jquery.lightbox-0.5"], function () {
     //鼠标移出灰色图片显示，彩色图片隐藏
    $block.on("mouseleave",leave);
 
-    //block10、block9鼠标滑过左右拉开
+    //block10、block9鼠标滑过左右拉开,文字溅隐溅显
     var $yunj=$(".yunj");
     var $pic9=$(".block_9 .pic");
     var $pic10=$(".block_10 .pic");
+    var $msg10=$(".block_10 .message .text");
+    var $msg9=$(".block_9 .message .text");
 
     $pic9.on("mouseenter",function(){
-        $pic10.css('left',"100%");
         $yunj.data("sld","pic10");
+        $pic10.css('left',"100%");
+        $msg10.fadeIn(2000);
     });
 
     $pic10.on("mouseenter",function(){
-        $pic9.css('left',"-100%");
         $yunj.data("sld","pic9");
+        $pic9.css('left',"-100%");
+        $msg9.fadeIn(2000);
+
     });
 
     $yunj.on("mouseleave",function(){
@@ -89,31 +88,28 @@ require(["jquery","hoverChangePic/jquery.lightbox-0.5"], function () {
         if(sld=="pic10")
         {
             $pic10.css('left',"0");
+            $msg10.fadeOut();
         }
      else if(sld=="pic9")
         {
             $pic9.css("left","0");
+            $msg9.fadeOut();
         }
     });
 
 
-
-   /* $yunj.on("mouseleave",function(){
-        $pic9.css('left',"0");
-    });*/
-
     //鼠标滑过logo商标变换图片
     $logoBlock.on("mouseenter",function(){
-        $(this).find(".showPic1").css("display","none").siblings(".showPic2").css("display","block");
+        $(this).find(".showPic2").css({"opacity":1});
     });
     $logoBlock.on("mouseleave",function(){
-        $(this).find(".showPic2").css("display","none").siblings(".showPic1").css("display","block");
+        $(this).find(".showPic2").css({"opacity":0});
     });
 
    function enter(){
        var $radio=$(this).find(".radio");
        //鼠标移入灰色图片隐藏，彩色图片显示
-       $(this).find(".showPic1").css("display","none").siblings(".showPic2").css("display","block");
+       $(this).find(".showPic2").css("opacity",1);
        $(this).find(".shade .bg").stop(true,true);
        $(this).find(".shade .tex").stop(true,true);
         //定义Deffered
@@ -128,13 +124,13 @@ require(["jquery","hoverChangePic/jquery.lightbox-0.5"], function () {
         //动画
        $(this).find(".shade .bg").animate({
             top:"0"
-        },600,function(){
+        },800,function(){
             //释放d1
             d1.resolve();
         });
        $(this).find(".shade .tex").animate({
             top:"0"
-        },600,function(){
+        },800,function(){
             //释放d2
             d2.resolve();
         })
@@ -142,7 +138,7 @@ require(["jquery","hoverChangePic/jquery.lightbox-0.5"], function () {
 
     function leave(){
         var $radio=$(this).find(".radio");
-        $(this).find(".showPic2").css("display","none").siblings(".showPic1").css("display","block");
+        $(this).find(".showPic2").css("opacity",0)/*.siblings(".showPic1").css("display","block")*/;
 
         $radio.stop(true,true);
         $(this).find(".shade .bg").stop(true,true);
